6905f165d6c2874353cdbe1aa5af165020546956
asiftyro <asif.tyro@gmail.com>
2021-09-15 20:58:18 +0600

    Timezone setup in wsgi

--- wsgi.sample.py
+++ wsgi.sample.py
@@ -1,10 +1,14 @@
 import os
 import sys
+import time
 
 path = '/home/your-username/your-project-directory-name'
 if path not in sys.path:
     sys.path.append(path)
 
 os.environ['FLASK_ENV'] = 'production'  # options: development, production
+os.environ["TZ"] = "Asia/Dhaka"
 
+time.tzset()
 from run import app as application
+
